251fe3a603c3bbae9a6aef4648e27744c57cf7ba
Isabella <isabella.cardoso1707@gmail.com>
2025-11-12 08:25:34 +0200

    Event screen styling

--- obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c27876bd2367e3e62baa57b59065c22ece326a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
